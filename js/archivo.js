c52a566dc7325e7695d3e75a2efcddacf8f738d9
user <mary20bw@gmail.com>
2017-03-05 16:04:09 -0500

    primer cambio

--- js/archivo.js
+++ js/archivo.js
@@ -1,14 +1,9 @@
 function resultado(){
-var peso = document.getElementById("peso").value;//al ser "string" convertir con parseInt o parseFloat
+var peso = document.getElementById("peso").value;
 var estatura = document.getElementById("estatura").value;
 console.log(peso);
-if(peso !== "" || estatura !== "" ){
-	var imc= peso/Math.pow(estatura,2);//var imc= peso.toFixed(2)/Math.pow(estatura,2) --> no imprime asi, por qué?? Al parecer porque los valores que se ingresasn son "string" y toFixed no funciona con ese tipo de variable :/
-	alert(imc.toFixed(2));
-}
-else
-alert("Error");
 
-//var imc = (peso/Math.pow(estatura,2));
+var imc= peso/Math.pow(estatura,2);
+	alert(imc.toFixed(2));
 
 }
